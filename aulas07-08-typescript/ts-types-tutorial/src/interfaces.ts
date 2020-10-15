371a5ded19677cb7cca629cd390d8ef9688de6a9
Prof. Diogo S. Martins <santana.martins@ufabc.edu.br>
2020-10-15 20:01:11 -0300

    aulas 05-08

---
+++ aulas07-08-typescript/ts-types-tutorial/src/interfaces.ts
@@ -0,0 +1,13 @@
+interface Aluno {
+    nome: string,
+    sobrenome?: string,
+    ra: string, 
+    idade: number
+}
+
+const a1: Aluno = {
+    nome: 'joao', 
+    sobrenome: 'silva',
+    ra: '1234',
+    idade: 25
+}
